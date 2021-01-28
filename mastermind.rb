87d4818d15907ae88f84ccd99ef5a5a3320c180b
Daniel Ash <d_ash12345@hotmail.co.uk>
2021-01-28 20:33:41 +0000

    Added logic to game.

--- mastermind.rb
+++ mastermind.rb
@@ -15,15 +15,24 @@ end
 class Logic < Layout
   def initialize
     super
-    @round_counter = 12
+    @round_counter = 11
+    @computer_combination = []
+  end
+
+  def new_combination
+    @computer_combination = generate_combination
+    p @computer_combination
   end
 
   def request_input
+    you_lose if @round_counter.zero?
     input = gets.chomp
-    check_valid_input(input)
-    p input
-    edit_layout(input.upcase)
-    puts "Your colors are: #{input.upcase}."
+    if check_valid_input(input) == 'invalid'
+      request_input
+    else
+      edit_layout(input.upcase)
+      evaluate_input(input.upcase.split)
+    end
   end
 
   def edit_layout(input)
@@ -41,18 +50,96 @@ class Logic < Layout
       'invalid'
     end
   end
-end
 
+  def evaluate_input(input)
+    puts "There are #{right_place(input)} colors in the right place and #{right_color(input)} right colors in the wrong place."
+    victory if right_place(input) == 4
+  end
+
+  def right_place(input)
+    right_place = 0
+    input.each_with_index do |letter, index|
+      if letter == @computer_combination[index]
+        right_place += 1
+      end
+    end
+    right_place
+  end
+
+  def right_color(input)
+    computer_count = []
+    player_count = []
+    input.each_with_index do |letter, index|
+      if letter != @computer_combination[index] && @computer_combination.include?(letter)
+        player_count.push(letter)
+        computer_count.push(@computer_combination[index])
+      end
+    end
+    total = computer_count & player_count
+    total.length
+  end
+
+  # def calc_right_color(array_one, array_two)
+  #   same_letters = 0
+  #   array_one.each do |letter|
+  #     same_letters += 1 if array_two.include?(letter)
+  #   end
+  #   return 0 if same_letters.zero?
+
+  #   # if sum_array(array_one) <= sum_array(array_two)
+  #   #   sum_array(array_one)
+  #   # else
+  #   #   sum_array(array_two)
+  #   # end
+  #   p array_one & array_two
+  # end
+
+  private
+
+  def generate_combination
+    rand_array = []
+    4.times do
+      rand_array.push(rand(6))
+    end
+    rand_array.map { |number| COLORS[number] }
+  end
+end
 
 class Play < Logic
   def play_game
+    playing_field = Layout.new
+    @round_counter = 11
     puts 'Welcome to Mastermind!'
-    create_field
-    12.times do
+    playing_field.create_field
+    new_combination
+    13.times do
       puts 'Please enter four colors.'
       request_input
     end
   end
+
+  def play_again
+    puts 'Would you like to play again? (y/n)'
+    input = gets.chomp.downcase
+    case input
+    when 'y' then play_game
+    when 'n' then exit
+    else 
+      puts "Please enter either 'y' (yes) or 'n' (no)."
+      play_again
+    end
+  end
+
+  def victory
+    puts "Congratulations, you win! It took you #{11 - @round_counter} guesses."
+    play_again
+  end
+
+  def you_lose
+    puts 'Unfortunately you have run out of guesses, so you lose. But why not try gain!'
+    play_again
+  end
+
 end
 
 test = Play.new

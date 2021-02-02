1abfdbdafa5b7d986b26fd2883548eb8e8dfc99a
Daniel Ash <d_ash12345@hotmail.co.uk>
2021-02-02 14:29:23 +0000

    Finished main functionality, will return to make small adjustments in the future

--- mastermind.rb
+++ mastermind.rb
@@ -16,12 +16,11 @@ class Logic < Layout
   def initialize
     super
     @round_counter = 11
-    @computer_combination = []
+    @combination = []
   end
 
   def new_combination
-    @computer_combination = generate_combination
-    p @computer_combination
+    @combination = generate_combination
   end
 
   def request_input
@@ -30,20 +29,28 @@ class Logic < Layout
     if check_valid_input(input) == 'invalid'
       request_input
     else
-      edit_layout(input.upcase)
+      edit_layout(input.upcase.split)
       evaluate_input(input.upcase.split)
     end
   end
 
+  def request_player_code
+    puts 'Please create a code for the computer to guess. You can use any of the following colors: R, G, B, Y, P, W.'
+    code = gets.chomp
+    check_valid_input(code)
+    @combination = code.split
+  end
+
   def edit_layout(input)
-    @playing_field[@round_counter] = input.split
+    @playing_field[@round_counter] = input
     create_field
     @round_counter -= 1
   end
 
   def check_valid_input(input)
-    if input.split.length != 4
-      puts 'Error: incorrect input length. Please enter four letters.'
+    if input == 'exit' then exit
+    elsif input.split.length != 4
+      puts 'Error: incorrect input length. Please enter four characters.'
       'invalid'
     elsif input.split.any? { |color| COLORS.include?(color) == false }
       puts 'Error: incorrect input. Please enter valid characters.'
@@ -52,47 +59,42 @@ class Logic < Layout
   end
 
   def evaluate_input(input)
-    puts "There are #{right_place(input)} colors in the right place and #{right_color(input)} right colors in the wrong place."
+    puts "Colors in the right place: #{right_place(input)}. Right colors, but in the wrong place: #{right_color(input)}."
     victory if right_place(input) == 4
   end
 
   def right_place(input)
     right_place = 0
     input.each_with_index do |letter, index|
-      if letter == @computer_combination[index]
+      if letter == @combination[index]
         right_place += 1
       end
     end
     right_place
   end
 
+  def wrong_place_index(input)
+    wrong_index = []
+    input.each_with_index do |letter, index|
+      if letter != @combination[index]
+        wrong_index.push(index)
+      end
+    end
+    wrong_index
+  end
+
   def right_color(input)
     computer_count = []
     player_count = []
     input.each_with_index do |letter, index|
-      if letter != @computer_combination[index] && @computer_combination.include?(letter)
+      if letter != @combination[index] && @combination.include?(letter)
         player_count.push(letter)
-        computer_count.push(@computer_combination[index])
+        computer_count.push(@combination[index])
       end
     end
     total = computer_count & player_count
     total.length
   end
-
-  # def calc_right_color(array_one, array_two)
-  #   same_letters = 0
-  #   array_one.each do |letter|
-  #     same_letters += 1 if array_two.include?(letter)
-  #   end
-  #   return 0 if same_letters.zero?
-
-  #   # if sum_array(array_one) <= sum_array(array_two)
-  #   #   sum_array(array_one)
-  #   # else
-  #   #   sum_array(array_two)
-  #   # end
-  #   p array_one & array_two
-  # end
 
   private
 
@@ -105,26 +107,85 @@ class Logic < Layout
   end
 end
 
-class Play < Logic
-  def play_game
-    playing_field = Layout.new
-    @round_counter = 11
+class Player < Logic
+end
+
+class Computer < Player
+  def computer_play
+    i = 1
+    n = initial_guess
+    until n == @combination
+      filter_guess(n, wrong_place_index(n))
+      i += 1
+    end
+    i
+  end
+
+  def filter_guess(guess, index_array)
+    index_array.each { |index| guess[index] = generate_letter }
+  end
+
+  def generate_letter
+    COLORS[rand(6)]
+  end
+
+  def initial_guess
+    rand_array = []
+    4.times do
+      rand_array.push(rand(6))
+    end
+    rand_array.map { |number| COLORS[number] }
+  end
+end
+
+class Play < Computer
+  def game_start
+    explain_rules
+    choose_game_mode
+  end
+
+  def explain_rules
     puts 'Welcome to Mastermind!'
-    playing_field.create_field
+    puts 'Which mode would you like to play?'
+    puts '1. you crack the computer\'s code'
+    puts '2. create your own code and have the computer crack it'
+  end
+
+  def choose_game_mode
+    choice = gets.chomp
+    case choice
+    when '1' then player_guesses
+    when '2' then computer_guesses
+    else
+      puts 'Please type in either 1 or 2.'
+      choose_game_mode
+    end
+  end
+
+  def player_guesses
+    @round_counter = 11
+    @playing_field = Array.new(12, %w[o o o o])
+    create_field
     new_combination
     13.times do
-      puts 'Please enter four colors.'
+      puts 'Please enter four colors. (Available colors are: R, G, B, Y, P, W)'
       request_input
     end
+  end
+
+  def computer_guesses
+    request_player_code
+    puts "It took the computer #{computer_play} guesses to break your code!"
+    play_again
   end
 
   def play_again
     puts 'Would you like to play again? (y/n)'
     input = gets.chomp.downcase
     case input
-    when 'y' then play_game
+    when 'y' then game_start
     when 'n' then exit
-    else 
+    else
       puts "Please enter either 'y' (yes) or 'n' (no)."
       play_again
     end
@@ -139,8 +200,7 @@ class Play < Logic
     puts 'Unfortunately you have run out of guesses, so you lose. But why not try gain!'
     play_again
   end
-
 end
 
 test = Play.new
-test.play_game
+test.game_start
